e7b80ed9b1513a017c71ca236b9921e14a127b91
michedomingo <michedomingo@gmail.com>
2020-05-28 09:30:47 -0700

    Returns definition of word, implementing case sensitivity

--- python-final_project-dictionary/dictionary.py
+++ python-final_project-dictionary/dictionary.py
@@ -6,14 +6,18 @@ import json
 
 
 def main():
-	data = json.load(open('data.json'))
-	print(data)
+	dict_data = json.load(open('data.json'))
+	word = input('Enter word: ')
+	word = validate(word.lower(), dict_data)
+	translate(word, dict_data)
 
-	# validate(word.lower())
+def validate(key, data):
+	if key in data.keys():
+		return key
 
-
-
-
+def translate(key, data):
+	print('\n'.join("{}".format(info) for info in data.get(key)))
+	return
 
 if __name__ == '__main__':
 	main()
