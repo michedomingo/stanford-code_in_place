28bb2d99d59e2758e8338912f3606c6446c03868
michedomingo <michedomingo@gmail.com>
2020-05-30 05:13:51 -0700

    Update output

--- python-final_project-dictionary/dictionary.py
+++ python-final_project-dictionary/dictionary.py
@@ -8,12 +8,14 @@ import difflib
 
 def main():
     dict_data = json.load(open('data.json'))
-    word = input('Enter word: ')
-    word = update_case(word, dict_data)
-    word = validate(word, dict_data)
-    if word is False:
-        return
-    translate(word, dict_data)
+    while True:
+        word = input('\nEnter word: ')
+        word = update_case(word, dict_data)
+        word = validate(word, dict_data)
+        if word is False:
+            pass
+        else:
+            translate(word, dict_data)
 
 
 def update_case(key, data):
@@ -36,23 +38,23 @@ def validate(key, data):
     elif len(difflib.get_close_matches(key, data)) > 0:
         while True:
             alt_word = difflib.get_close_matches(key, data.keys(), n=1)[0]
-            print('Did you mean {} instead? '.format(alt_word))
+            print('\nDid you mean {} instead? '.format(alt_word))
             action = input('Enter Y if yes, or N if no: ')
             if action == 'Y':
                 word = difflib.get_close_matches(key, data.keys(), n=1)[0]
                 return word
             elif action == 'N':
-                print('The word doesn\'t exist. Please double check it.')
+                print('\n\tHmmm...no results found for {}. Please spell check and try again. 🤓'.format(key))
                 return False
             else:
-                print('Try again. Y or N')
+                print('\n\tTry again. Y or N')
     else:
-        print('The word doesn\'t exist. Please double check it.')
+        print('\n\tHmmm...no results found for {}. Please spell check and try again. 🤓'.format(key))
         return False
 
 
 def translate(key, data):
-    print('\n'.join("{}".format(info) for info in data.get(key)))
+    print('\n'.join("\n\t{}".format(info) for info in data.get(key)))
     return
 
 
